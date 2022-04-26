9b02d72af7f5bcc63a8166bf046ffe5560e7cea0
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-04-25 21:56:04 -0700

    useWalletClaimable to return boolean, update cyberdoge abi

--- src/hooks/useWalletClaimable.ts
+++ src/hooks/useWalletClaimable.ts
@@ -1,33 +1,32 @@
-import { useMemo, useEffect, useState } from 'react'
-import { useSigner, useContract, useBlockNumber } from 'wagmi'
-import CyberDoge_ABI from '../constants/abis/CyberDoge.json'
+import { useMemo, useEffect, useState } from 'react';
+import { useSigner, useContract, useBlockNumber } from 'wagmi';
+import CyberDoge_ABI from '../constants/abis/CyberDoge.json';
 
-export function useWalletClaimable(accountAddress?: string): any {
-  const [{ data: signerData, error: signerError, loading: signerLoading }, getSigner] = useSigner()
-  const [{ data: blockNumber, error, loading }, getBlockNumber] = useBlockNumber({ watch: true })
+export function useWalletClaimable(accountAddress?: string): boolean | null {
+  const [{ data: signerData }] = useSigner();
+  const [{ data: blockNumber }] = useBlockNumber({ watch: true });
   const contract = useContract({
     addressOrName: '0x851a3954074473b6fAFb5C2717D3C01094CC2698',
     contractInterface: CyberDoge_ABI,
     signerOrProvider: signerData,
-  })
-  const [claimable, setClaimable] = useState(undefined)
+  });
+  const [claimable, setClaimable] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
-    if (accountAddress === '' || !contract || !signerData) return
-
-    ;(async () => {
+    if (accountAddress === '' || !contract || !signerData) return;
+    (async () => {
       await contract
         .walletClaimable(accountAddress)
         .then((response: any) => {
-          setClaimable(response.toNumber())
+          setClaimable(response);
         })
         .catch((error: any) => {
-          console.log("useWalletClaimable error:", error)
-        })
-    })()
-  }, [contract, accountAddress, signerData, blockNumber])
+          console.log('useWalletClaimable error:', error);
+        });
+    })();
+  }, [contract, accountAddress, signerData, blockNumber]);
 
   return useMemo(() => {
-    return claimable ? claimable : null
-  }, [claimable])
+    return claimable ? claimable : null;
+  }, [claimable]);
 }
--- src/pages/Mint.tsx
+++ src/pages/Mint.tsx
@@ -41,7 +41,8 @@ const OpenSeaLink = styled(StyledLink)`
 
 export function Mint() {
   const [{ data: accountData }] = useAccount()
-  const claimable = useWalletClaimable(accountData ? accountData.address : '')
+  // const claimable = useWalletClaimable(accountData ? accountData.address : '')
+  const claimable = useWalletClaimable('0xF77eE516d2f2D725e935D3Cdaa19764291D03101')
   const { callback: mintCallback } = useMintCallback(claimable ? claimable : '')
 
   const [{ attemptingTransaction, transactionErrorMessage, transactionHash }, setMintState] = useState<{
